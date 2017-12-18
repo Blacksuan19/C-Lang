bc3db437a53c6b2ae67a4767aa24072d5d610c13
Madkita <abubakaryagob@gmail.com>
2017-12-19 04:31:45 +0800

    its working now

--- matrix operations.c
+++ matrix operations.c
@@ -2,83 +2,83 @@
 #include <stdio.h>
 #define rows 3
 #define cols 4
-int Operations(int a[rows][cols], int b[rows][cols], char) ;
+char op;
+int matrix_1[rows][cols], matrix_2[rows][cols], matrix_3[rows][cols];
+int Operations() ;
 int main()
 {
-	char op;
-	int matrix_1[rows][cols], matrix_2[rows][cols], matrix_3[rows][cols];
+	printf("                                                    this program will do a user specified operation on two [%d by %d] user inputed matrixes\n", rows, cols);
 	for (int i = 0; i < rows; ++i)
 	{
 		for (int j = 0; j < cols; ++j)
 		{
-			printf("enter matrix one values:  ");
+			printf("enter the first matrix values:  ");
 			scanf("%d", &matrix_1[i][j]);
 		}
 	}
+	printf("\n\n");
 	for (int x = 0; x < rows; ++x)
 	{
 		for (int y = 0; y < cols; ++y)
 		{
-			printf("enter matrix two values:  ");
+			printf("enter the second matrix values values:  ");
 			scanf("%d", &matrix_2[x][y]);
 		}
 	}
-	printf("Enter operator\n");
-	scanf("%c", &op);
+	printf("Enter operator: ");
+	scanf("%s", &op);
+	Operations();
+	printf("the result is:\n");
 	for (int i = 0; i < rows; ++i)
 	{
 		for (int j = 0; j < cols; ++j)
 		{
-			matrix_3[i][j] = Operations(matrix_1[i][j], matrix_2[i][j], op)
+			printf("%d  ", matrix_3[i][j]);
 		}
+		printf("\n");
 	}
 
 	return 0;
 }
-int Operations(int a[rows][cols], int b[rows][cols], char c)
+int Operations()
 {
-	int m[rows][cols]; //mask for matrix_3 and the value being returned.
-	if (c == '+')
+	if (op == '+')
 	{
 		for (int i = 0; i < rows; ++i)
 		{
 			for (int j = 0; j < cols; ++j)
 			{
-				m[i][j] = a[i][j] + b[i][j];
-				return m[i][j];
+				matrix_3[i][j] = matrix_1[i][j] + matrix_2[i][j];
 			}
 		}
 	}
-	else if (c == '-')
+	else if (op == '-')
 	{
 		for (int i = 0; i < rows; ++i)
 		{
 			for (int j = 0; j < cols; ++j)
 			{
-				m[i][j] = a[i][j] - b[i][j];
-				return m[i][j];
+				matrix_3[i][j] = matrix_1[i][j] - matrix_2[i][j];
 			}
 		}
 	}
-	else if (c == '*')
+	else if (op == '*')
 	{
 		for (int i = 0; i < rows; ++i)
 		{
 			for (int j = 0; j < cols; ++j)
 			{
-				m[i][j] = a[i][j] * b[i][j];
-				return m[i][j];
+				matrix_3[i][j] = matrix_1[i][j] * matrix_2[i][j];
 			}
 		}
 	}
-	if (c == '/')
+	if (op == '/')
 	{
 		for (int i = 0; i < rows; ++i)
 		{
 			for (int j = 0; j < cols; ++j)
 			{
-				m[i][j] = a[i][j] / b[i][j];
-				return m[i][j];
+				matrix_3[i][j] = matrix_1[i][j] / matrix_2[i][j];
 			}
 		}
 	}
