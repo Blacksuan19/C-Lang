8c31b17ef454ae2637808b974ff6907381c5aaa2
Madkita <abubakaryagob@gmail.com>
2017-12-19 02:56:56 +0800

    adjustments

--- assignment 2.c
+++ assignment 2.c
@@ -10,10 +10,10 @@ Group Members:
 #include <stdio.h>
 #define PI 3.142 // pi value 
 //global variable definition so they can be used across all the functions.
-int dia_big, dia_small, a;
+int b, s; // b for big diameter and s for the small one.
 float price_s, price_b, area, unit_price_b, unit_price_s;
-//functions and their jobs!.
-int GetDiameter(); 	//get the pizza diameter.
+//functions and tasks
+int GetDiameter();  // get the pizza diameter.
 float Compare(float, float); // compare the two price units.
 float GetPrice(); // get the pizza price.
 float CalclulateUnitPrice(int, float);  //calculate the unit price using diameter and price.
@@ -21,23 +21,23 @@ int main()
 {
 	printf("Welcome To IIUM Pizza\n\n");
 	printf("Enter diameter of small pizza <in inches>: ");
-	dia_small = GetDiameter(); //function call.
+	s = GetDiameter(); //function call.
 	printf("Enter diameter of big pizza <in inches>: ");
-	dia_big = GetDiameter();
+	b = GetDiameter();
 	//now we get the price
 	GetPrice();
 	//now will calculate the unit price.
-	unit_price_s = CalclulateUnitPrice(dia_small, price_s); //function call.
-	unit_price_b = CalclulateUnitPrice(dia_big, price_b); //function call.
+	unit_price_s = CalclulateUnitPrice(s, price_s); //function call.
+	unit_price_b = CalclulateUnitPrice(b, price_b); //function call.
 	Compare(unit_price_b, unit_price_s);
 	return 0;
 }
 int GetDiameter()
 {
-	int a;
+	int d;
 	//get the pizza diameter
-	scanf("%d", &a);
-	return a; // return the user inputed value
+	scanf("%d", &d);
+	return d; // return the user inputed value
 }
 float GetPrice()
 {
