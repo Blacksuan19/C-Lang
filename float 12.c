4313f61e293752ef75d1b71df43adb21b94bd3f8
Madkita <abubakaryagob@gmail.com>
2017-12-27 09:39:31 +0800

    another array thing

---
+++ float 12.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define SIZE 3
+float Deci_num[SIZE], largest, average, total;
+void OP();
+float Largest();
+int main()
+{
+	printf("enter 12 floating point numbers?\n");
+	for (int i = 0; i < SIZE; ++i)
+	{
+		scanf("%f", &Deci_num[i]);
+	}
+	OP();
+	printf("total is %.2f\n", total);
+	printf("average is %.2f\n", average);
+	printf("largest value is %.2f\n", largest);
+	return 0;
+}
+void OP()
+{
+	for (int i = 0; i < SIZE; ++i)
+	{
+		total += Deci_num[i];
+	}
+	for (int i = 0; i < SIZE; ++i)
+	{
+		average = (average + Deci_num[i]) / 2;
+	}
+	largest = Deci_num[0];
+	for (int i = 1; i < SIZE; ++i)
+	{
+		if (largest < Deci_num[i])
+		{
+			largest = Deci_num[i];
+		}
+	}
+}
